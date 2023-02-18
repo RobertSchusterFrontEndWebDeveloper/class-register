7809b9c0200207a5b02eab09736d7c1120181a4c
Rob Schuster <robert.is@icloud.com>
2023-02-17 22:02:58 -0500

    Complete parse of data, sort registered and not registered, display not registered

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const notRegistered = document.querySelector(".not-registered");
+
+const getRegistrationData = async function () {
+  const request = await fetch(`https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json`);
+  const data = await request.json();
+
+  console.log("got here");
+  displayContactList(data);
+}
+
+const displayContactList = function(data) {
+  notRegistered.innerHTML = "";
+
+  for (const student of data) {
+    const name = student.name;
+    const registered = student.registered;
+    console.log(`${name}`);
+    console.log(`${registered}`);
+
+    if (registered === "no") {
+      let li = document.createElement("li");
+      li.innerHTML = `${name}`;
+      notRegistered.append(li);
+    } 
+  } 
+}
+
+getRegistrationData();
