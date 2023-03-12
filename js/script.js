988b2610c0563093331b0208f6ee8d87213aced8
Rob Schuster <robert.is@icloud.com>
2023-03-12 17:33:06 -0400

    Changed wording in first consolelog

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const getRegistrationData = async function () {
   const request = await fetch(`https://gist.githubusercontent.com/skillcrush-curriculum/4bfa96d3762f86d2327d389063b3ea8d/raw/86e50e48e98c662ead6d9bd191ef7db381e9f4b4/fieldtrip.json`);
   const data = await request.json();
 
-  console.log("got here");
+  console.log("Json file retrieved");
   displayContactList(data);
 }
 
